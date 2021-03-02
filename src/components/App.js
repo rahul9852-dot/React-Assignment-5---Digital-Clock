676419db9aefb52f26b225f9b1be03b6cad0c5f6
rahul9852-dot <66193581+rahul9852-dot@users.noreply.github.com>
2021-03-02 15:33:04 +0530

    Digital Clock

--- src/components/App.js
+++ src/components/App.js
@@ -1,16 +1,63 @@
-import React, {Component, useState} from "react";
-import '../styles/App.css';
 
-class App extends Component {
-    render() {
+import "./styles.css";
 
-        return(
-            <>
-               
-            </>
-        )
+import React from "react";
+
+class App extends React.Component{
+
+  constructor(){
+    super()
+    this.state={
+      time:new Date().toLocaleTimeString()
     }
-}
 
+    this.DateFunction=this.DateFunction.bind(this)
+  }
+
+  DateFunction(){
+    
+  let date = new Date()
+
+  let updateHour = date.getHours()
+ 
+  let updateMinute = date.getMinutes()
+ 
+  let updateSecond = date.getSeconds()
+ 
+  
+ 
+ let updateAMPM = updateHour < 13 ? 'AM' : 'PM'
+ 
+  if(updateMinute < 10) updateMinute = `0${updateMinute}`
+ 
+  if(updateSecond < 10) updateSecond = `0${updateSecond}`
+ 
+  let updateTime = `${updateHour}:${updateMinute}:${updateSecond} ${updateAMPM}`
+ 
+  
+ 
+ this.setState({time: updateTime})
+  }
+
+
+  componentDidMount(){
+    this.loadInterval=setInterval(this.dateFunction,1000)
+  }
+
+  componentDidCatch(){
+    clearInterval(this.loadInterval)
+  }
+
+  render(){
+    return(
+      <div>
+        <div className="Clock">
+
+        <h3 id="time">{this.state.time}</h3>
+        </div>
+      </div>
+    )
+  }
+}
 
 export default App;

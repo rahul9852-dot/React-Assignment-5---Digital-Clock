8b68ff526356255f2641ce9c17f1d8774fd9e0dc
rahul9852-dot <66193581+rahul9852-dot@users.noreply.github.com>
2021-03-02 15:38:01 +0530

    again updated

--- src/components/App.js
+++ src/components/App.js
@@ -1,63 +1,95 @@
+import React, {Component, useState} from "react";
 
-import "./styles.css";
+import './styles.css';
 
-import React from "react";
-
-class App extends React.Component{
-
-  constructor(){
-    super()
-    this.state={
-      time:new Date().toLocaleTimeString()
-    }
-
-    this.DateFunction=this.DateFunction.bind(this)
-  }
-
-  DateFunction(){
-    
-  let date = new Date()
-
-  let updateHour = date.getHours()
  
-  let updateMinute = date.getMinutes()
- 
-  let updateSecond = date.getSeconds()
- 
-  
- 
- let updateAMPM = updateHour < 13 ? 'AM' : 'PM'
- 
-  if(updateMinute < 10) updateMinute = `0${updateMinute}`
- 
-  if(updateSecond < 10) updateSecond = `0${updateSecond}`
- 
-  let updateTime = `${updateHour}:${updateMinute}:${updateSecond} ${updateAMPM}`
- 
-  
- 
- this.setState({time: updateTime})
-  }
 
+class App extends Component {
 
-  componentDidMount(){
-    this.loadInterval=setInterval(this.dateFunction,1000)
-  }
+ constructor() {
 
-  componentDidCatch(){
-    clearInterval(this.loadInterval)
-  }
+ super()
 
-  render(){
-    return(
-      <div>
-        <div className="Clock">
+ this.state = {
 
-        <h3 id="time">{this.state.time}</h3>
-        </div>
-      </div>
-    )
-  }
+ time: new Date().toLocaleTimeString()
+
+ }
+
+ 
+
+this.dateFunction = this.dateFunction.bind(this)
+
+ }
+
+ 
+
+dateFunction() {
+
+ let date = new Date()
+
+ let updateHour = date.getHours()
+
+ let updateMinute = date.getMinutes()
+
+ let updateSecond = date.getSeconds()
+
+ 
+
+let updateAMPM = updateHour < 13 ? 'AM' : 'PM'
+
+ if(updateMinute < 10) updateMinute = `0${updateMinute}`
+
+ if(updateSecond < 10) updateSecond = `0${updateSecond}`
+
+ let updateTime = `${updateHour}:${updateMinute}:${updateSecond} ${updateAMPM}`
+
+ 
+
+this.setState({time: updateTime})
+
+ }
+
+ 
+
+componentDidMount() {
+
+ this.loadInterval = setInterval(this.dateFunction, 1000)
+
+ }
+
+ 
+
+componentWillUnmount() {
+
+ clearInterval(this.loadInterval)
+
+ }
+
+ 
+
+render() {
+
+ 
+
+return(
+
+ <>
+
+ <div className="Clock">
+
+ <h3 id="time">{this.state.time}</h3>
+
+ </div>
+
+ </>
+
+ )
+
+ }
+
 }
+
+ 
 
 export default App;
